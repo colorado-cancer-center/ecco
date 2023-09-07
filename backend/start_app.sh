005b9e28ed090290c3a1595a591755af6c86f4bb
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2023-09-07 10:29:55 -0600

    Added simplified tract geometry, modified start_app.sh to load simple geom if USE_HIGH_RES_TRACTS != 1

--- backend/start_app.sh
+++ backend/start_app.sh
@@ -5,26 +5,43 @@ IS_DEV=${IS_DEV:-0}
 APP_HOST="0.0.0.0"
 APP_PORT="8000"
 
+# if 1, uses high-resolution tracts, but at the cost of higher memory/data usage
+USE_HIGH_RES_TRACTS=${USE_HIGH_RES_TRACTS:-0}
+
+
+# ========================================
+# === geometry import via ogr2ogr
+# ========================================
+
 # import counties
 ogr2ogr -f "PostgreSQL" PG:"host=${POSTGRES_HOST} dbname=${POSTGRES_DATABASE} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}" \
     /data/geometry/Colorado_County_Boundaries.geojson \
     -nln county -overwrite
 
-# import tracts
-ogr2ogr -f "PostgreSQL" PG:"host=${POSTGRES_HOST} dbname=${POSTGRES_DATABASE} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}" \
-    /data/geometry/Colorado_Census_Tract_Boundaries.geojson \
-    -nln tract -overwrite
+if [ ${USE_HIGH_RES_TRACTS} = "1" ]; then
+    # import tracts
+    ogr2ogr -f "PostgreSQL" PG:"host=${POSTGRES_HOST} dbname=${POSTGRES_DATABASE} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}" \
+        /data/geometry/Colorado_Census_Tract_Boundaries.geojson \
+        -nln tract -overwrite
+else
+    # import simplified tracts
+    ogr2ogr -f "PostgreSQL" PG:"host=${POSTGRES_HOST} dbname=${POSTGRES_DATABASE} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}" \
+        /data/geometry/Colorado_Census_Tract_Boundaries_lessmid.geojson \
+        -nln tract -overwrite
+fi
+
+
+# ========================================
+# === catch up on migrations
+# ========================================
 
 # apply migrations at startup
 alembic upgrade head
 
-# load up CO county boundaries from reference data
-# ogr2ogr -f "PostgreSQL" PG:"host=${POSTGRES_HOST} dbname=${POSTGRES_DATABASE} user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}" \
-#     /data/geometry/Colorado_County_Boundaries.geojson \
-#     -nln co_county_boundaries -overwrite
 
-# note that alembic must have already created the county and tract tables
-# for the imports below to work
+# ========================================
+# === start the app
+# ========================================
 
 if [ "${IS_DEV}" = "1" ]; then
     uvicorn main:app --reload --host "${APP_HOST}" --port "${APP_PORT}"
