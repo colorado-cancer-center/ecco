6ab5e108665882196b3aaabffd03b0bb7cad5ec9
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2024-03-13 16:02:44 -0600

    Hotfix 2: changed env var to DOMAIN_NAMES to illustrate there can be more than one, other minor changes to nginx

--- services/nginx-certbot/entrypoint.sh
+++ services/nginx-certbot/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+export PRIMARY_DOMAIN=$(echo $DOMAIN_NAME | cut -d' ' -f1)
+
 cron && /docker-entrypoint.sh "$@"
--- services/nginx-certbot/scripts/renew_cert_reload.sh
+++ services/nginx-certbot/scripts/renew_cert_reload.sh
@@ -5,7 +5,7 @@
 # if we received more than one space-delimited domain name,
 # each of them becomes a '--domain <X>' argument; we end up
 # with a cert that can answer for any of them
-DOMAIN_ARGS=$( echo "${DOMAIN_NAME}" | xargs -n 1 echo --domain | paste -sd ' ' )
+DOMAIN_ARGS=$( echo "${DOMAIN_NAMES}" | xargs -n 1 echo --domain | paste -sd ' ' )
 
 certbot certonly \
     --nginx --non-interactive --agree-tos \
