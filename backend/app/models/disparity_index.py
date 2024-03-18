4f8fb533cb59bd03c2502d44f6eb2e5f40bdc929
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2024-03-18 13:47:35 -0600

    Added disparity index model, migration, merge w/previous migration. Refactored models so that all stats-type models are in a 'base' module rather than in 'cif'. STATS_MODELS are now registered in the base module rather than cif.

---
+++ backend/app/models/disparity_index.py
@@ -0,0 +1,32 @@
+"""
+Models derived from disparity index data similar to CDPHE. Note that this
+data is about *cancer disparities*, not socioeconomic disparities as in
+the CIF data.
+
+See https://github.com/colorado-cancer-center/ecco/issues/27 for details.
+
+Note that since they're all at the county level, we leave out putting
+"county" in the name.
+"""
+
+from .base import MeasuresByCounty
+
+# ===========================================================================
+# === models from the CDPHE disparity index data
+# ===========================================================================
+
+class CancerDisparitiesIndex(MeasuresByCounty, table=True):
+    class Config:
+        label = "Cancer Disparities Index"
+
+# ===========================================================================
+# === model lists, metadata for downstream use
+# === (e.g. creating routes automatically, adding human-readable labels)
+# ===========================================================================
+
+DISPARITY_MODELS = {
+    "county": [
+        CancerDisparitiesIndex
+    ],
+    "tract": []
+}
