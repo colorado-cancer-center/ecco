a37add88ffb69404559fa3c88463ba8f735446ea
Vincent Rubinetti <vince.rubinetti@gmail.com>
2024-04-30 14:35:04 -0400

    add analytics

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import VueGtag from "vue-gtag";
 import VueTippy from "vue-tippy";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { router } from "@/pages";
@@ -11,6 +12,14 @@ import "./styles.css";
 createApp(App)
   .component("font-awesome-icon", FontAwesomeIcon)
   .use(router)
+  .use(
+    VueGtag,
+    {
+      config: { id: "G-XESEVBEL2X" },
+      enabled: window.location.hostname === "ecco.cu-dbmi.dev",
+    },
+    router,
+  )
   .use(VueTippy, tippyOptions)
   .directive("stop", stop)
   .mount("#app");
