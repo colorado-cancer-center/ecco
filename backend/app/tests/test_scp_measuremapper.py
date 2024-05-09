c813900879ce4853b6458f78b594ccfba3daa6dc
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2024-05-09 17:29:40 -0600

    Fixed MeasureMapper to handle various ways to get items out of dict-likes, converted to Mapping so it's read-only, and added a few tests to make sure things are working ok.

--- backend/app/tests/test_scp_measuremapper.py
+++ backend/app/tests/test_scp_measuremapper.py
@@ -1,19 +1,48 @@
+import pytest
+
 import sys
 sys.path.append("/app")
 
 from models.base import MeasureUnit
 from models.scp import SCP_MEASURE_DESCRIPTIONS
 
+from tools.collections import MeasureMapper
+
 def test_scp_mapped_measures():
-    from app.tools.collections import MeasureMapper
-
+    """
+    Spot-check the MeasureMapper objects in SCP_MEASURE_DESCRIPTIONS
+    and ensure we're getting the right units back, both via __getitem__()
+    and via get().
+    """
     for model, mapper in SCP_MEASURE_DESCRIPTIONS.items():
-        # ensure it's dict-like
-        assert isinstance(mapper, dict)
-
         # ensure that trends give us ordinal values (e.g., falling, stable, rising)
         # and that everything else gives us rates (i.e., incindence, mortality rates per 100k)
         expected_unit = MeasureUnit.ORDINAL if "trend" in model else MeasureUnit.RATE
+
+        assert mapper["All Cancer Sites"]["unit"] is not None
         assert mapper["All Cancer Sites"]["unit"] == expected_unit
 
+        assert mapper["All Cancer Sites"].get("unit") is not None
+        assert mapper["All Cancer Sites"].get("unit") == expected_unit
+
     
+def test_measuremapper_immutable():
+    """
+    Ensure that we can't set items on a MeasureMapper object.
+    """
+    mapper = MeasureMapper(MeasureUnit.RATE)
+
+    with pytest.raises(TypeError):
+        mapper["foo"] = "bar"
+    
+    with pytest.raises(AttributeError):
+        mapper.set("foo", "bar")
+
+def test_measuremapper_stringification():
+    """
+    Test str/repr methods on MeasureMapper.
+    """
+    mapper = MeasureMapper(MeasureUnit.RATE)
+
+    assert str(mapper) == "MeasureMapper(default_unit=MeasureUnit.RATE)"
+    assert repr(mapper) == "MeasureMapper(default_unit=MeasureUnit.RATE)"
--- backend/app/tools/collections.py
+++ backend/app/tools/collections.py
@@ -3,9 +3,11 @@ Utilities for producing things that look like collections,
 but generate the values on the fly.
 """
 
+from collections.abc import Mapping
+
 from models.base import MeasureUnit
 
-class MeasureMapper(dict):
+class MeasureMapper(Mapping):
     """
     A dict-like that, given a key, produces a result like { label: <key>, unit:
     <default_unit> }
@@ -14,15 +16,32 @@ class MeasureMapper(dict):
     its label are the same, and the unit is shared across lots of different
     measures.
     """
-    
-    def __init__(self, default_unit:MeasureUnit):
+
+    def __init__(self, default_unit: MeasureUnit):
         self.default_unit = default_unit
 
     def __getitem__(self, key):
-        return {
-            "label": key,
-            "unit": self.default_unit,
-        }
+        return {"label": key, "unit": self.default_unit}
+    
+    def get(self, key, default=None):
+        return {"label": key, "unit": self.default_unit}
+    
+    def __len__(self):
+        """
+        Returns 0, since the object has virtual keys and thus
+        there's nothing to count.
+        """
+        return 0
 
-    def __setitem__(self, *args, **kwargs) -> None:
-        raise NotImplementedError("MeasureMapper is read-only")
+    def __iter__(self):
+        """
+        Returns an empty iterator, since the object has virtual keys and thus
+        there's nothing to iterate over.
+        """
+        return iter([])
+    
+    def __repr__(self):
+        return f"MeasureMapper(default_unit={self.default_unit})"
+    
+    def __str__(self):
+        return repr(self)
