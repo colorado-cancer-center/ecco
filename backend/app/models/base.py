8c53687c1110831561888f23c4710c2d29af664c
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2024-12-05 11:24:03 -0700

    Adds WIP VapingHealthRegion model w/parent MeasuresByHealthRegion, metadata, migrations

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -55,6 +55,13 @@ class CancerStatsByCounty(BaseStatsModel):
 class MeasuresByTract(MeasuresByCounty):
     Tract: Optional[str] = Field(index=True, nullable=True)
 
+class MeasuresByHealthRegion(BaseStatsModel):
+    hs_region : str = Field(index=True)
+    FIPS : str = Field(index=True)
+    State : str = Field(index=True, foreign_key="us_state.name")
+    measure : str = Field(index=True)
+    value : float = Field(nullable=True)
+
 
 # ---------------------------------------------------------------------------
 # -- metadata about the models
@@ -105,6 +112,11 @@ from .scp import (
     SCP_FACTOR_DESCRIPTIONS,
     SCP_CANCER_MODELS,
 )
+from .vaping import (
+    VAPING_MODELS,
+    VAPING_MEASURE_DESCRIPTIONS,
+    VAPING_FACTOR_DESCRIPTIONS,
+)
 
 STATS_MODELS = {
     "county": (
@@ -119,7 +131,10 @@ STATS_MODELS = {
         DISPARITY_INDEX_MODELS["tract"] +
         RADON_MODELS["tract"] +
         SCP_MODELS["tract"]
-    )
+    ),
+    "healthregion": (
+        VAPING_MODELS["healthregion"]
+    ),
 }
 
 # to match the input schema, cancer models have columns named "Site" and "AAR"
@@ -135,10 +150,12 @@ MEASURE_DESCRIPTIONS = {
     **DISPARITY_INDEX_MEASURE_DESCRIPTIONS,
     **RADON_MEASURE_DESCRIPTIONS,
     **HPV_MEASURE_DESCRIPTIONS,
+    **VAPING_MEASURE_DESCRIPTIONS,
 }
 
 FACTOR_DESCRIPTIONS = {
     **CIF_FACTOR_DESCRIPTIONS,
     **SCP_FACTOR_DESCRIPTIONS,
     **HPV_FACTOR_DESCRIPTIONS,
+    **VAPING_FACTOR_DESCRIPTIONS,
 }
