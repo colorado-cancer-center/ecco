3121bf5d45c3df2fdbd0260092a9bee085032a24
Faisal Alquaddoomi <faisal.alquaddoomi@cuanschutz.edu>
2025-03-07 08:57:11 -0700

    Adds overwrite option for db export script to avoid interactive input

--- db-exports/make_db_export.sh
+++ db-exports/make_db_export.sh
@@ -6,12 +6,16 @@
 TARGET_DUMPFILE="${POSTGRES_DATABASE}_$( date +%Y-%m-%d).dump"
 
 if [ -f ${TARGET_DUMPFILE} ]; then
-    echo "* File ${TARGET_DUMPFILE} already exists, overwrite it?"
-    read -p "  (y/n) " -n 1 -r
-    echo
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        echo "* Aborting"
-        exit 1
+    if  [ ${FORCE_OVERWRITE} -ne 1 ]; then
+        echo "* File ${TARGET_DUMPFILE} already exists, overwrite it?"
+        read -p "  (y/n) " -n 1 -r
+        echo
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            echo "* Aborting"
+            exit 1
+        fi
+    else
+        echo "* Overwriting existing file ${TARGET_DUMPFILE}"
     fi
 fi
 
