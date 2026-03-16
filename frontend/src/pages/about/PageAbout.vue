756c2c00338cb1f260f46b943e3020f069d11e62
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-03-16 14:29:43 -0400

    Update citation and UCCC measure label (#166)

--- frontend/src/pages/about/PageAbout.vue
+++ frontend/src/pages/about/PageAbout.vue
@@ -43,13 +43,14 @@
     </p>
 
     <blockquote>
-      Jan T Lowery, Faisal Alquaddoomi, Vince Rubinetti, Todd Burus, Cydney T
-      Jardine, Adam C Warren, Jake M Walsh, Evelinn T Borrayo, Sean Davis.
+      Lowery JT, Alquaddoomi F, Rubinetti V, Burus T, Jardine C, Warren AC,
+      Walsh J, Borrayo E, Davis S.
       <br />
       Exploring Cancer in Colorado using a novel data platform: the ECCO
       experience.
       <br />
-      medRxiv 2026.02.03.26345489; doi.org/10.64898/2026.02.03.26345489
+      medRxiv (preprint): 2026.02.03.26345489, doi:
+      10.64898/2026.02.03.26345489, PMID: 41674638, PMCID: PMC12889761
     </blockquote>
 
     <p>
