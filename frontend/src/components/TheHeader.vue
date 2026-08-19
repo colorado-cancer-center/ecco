c10a4055ed1e93e252d7dc4a260da391ff409b35
Vincent Rubinetti <vince.rubinetti@gmail.com>
2026-08-19 16:39:54 -0400

    add share image, fix meta tags, add logo to header

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -7,7 +7,9 @@ import { routes } from "@/pages";
   <header
     class="flex flex-wrap items-center justify-between gap-2 p-4 max-md:flex-col max-md:text-center"
   >
-    <div class="flex items-baseline gap-4 text-black">
+    <div
+      class="flex flex-wrap items-center justify-center gap-x-4 gap-y-2 text-black"
+    >
       <AppLink to="/">
         <b>E</b><span class="font-light">xploring</span> <b>C</b
         ><span class="font-light">ancer in</span> <b>Co</b
@@ -15,8 +17,9 @@ import { routes } from "@/pages";
       </AppLink>
       <AppLink
         to="https://medschool.cuanschutz.edu/colorado-cancer-center"
-        class="text-xs font-light"
+        class="flex items-center gap-2 text-xs font-light"
       >
+        <img src="/icon.png" class="h-5" alt="" />
         University of Colorado Cancer Center
       </AppLink>
     </div>
